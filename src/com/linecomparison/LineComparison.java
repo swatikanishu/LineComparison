e9d21bca5a4e91894efa81d1b06e8c253792255f
swatikanishu <nishuswatika@gmail.com>
2022-06-18 22:26:35 +0530

    Added: Line Functionality Added

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class LineComparison {
         public static void main(String[] args) {
-            int x1, x2, y1, y2;
-            double length;
+            int x1, x2, y1, y2, x3, y3, x4, y4
+            double length, length2
             System.out.println("Welcome to Line Comparison Computation Program");
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter first two co-ordinates:");
@@ -15,6 +15,14 @@ public class LineComparison {
             y2=sc.nextInt();
             length = Math.sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2);
             System.out.println("Length of the line:" + length);
+            System.out.println("Enter third co-ordinates:");
+            x3 = sc.nextInt();
+            y3 = sc.nextInt();
+            System.out.println("Enter fourth co-ordinates:");
+            x4 = sc.nextInt();
+            y4 = sc.nextInt();
+            length2 = Math.sqrt(Math.pow((x3 - y3), 2) + Math.pow((x4 - y4), 2));
+            System.out.println("Length of the line:" + length2);
             sc.close();
         }
 }
