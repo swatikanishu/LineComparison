e090862938f5b38fcdd9939023785e4e2c46a1a8
swatikanishu <nishuswatika@gmail.com>
2022-06-18 22:10:45 +0530

    Added: Welcome Message Added

---
+++ src/com/linecomparison/LineComparison.java
@@ -0,0 +1,9 @@
+package com.linecomparison;
+
+public class LineComparison {
+        public static void main(String[] args) {
+            int x1, x2, y1, y2;
+            double length;
+            System.out.println("Welcome to Line Comparison Computation Program");
+        }
+}
