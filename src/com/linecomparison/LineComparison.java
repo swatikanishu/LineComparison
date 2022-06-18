b077a3a37582e5be5f51bb1b2fc89c1c732ce330
swatikanishu <nishuswatika@gmail.com>
2022-06-18 22:17:02 +0530

    Added: Line Length Added

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -1,9 +1,20 @@
 package com.linecomparison;
+import java.util.Scanner;
 
 public class LineComparison {
         public static void main(String[] args) {
             int x1, x2, y1, y2;
             double length;
             System.out.println("Welcome to Line Comparison Computation Program");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter first two co-ordinates:");
+            x1=sc.nextInt();
+            y1=sc.nextInt();
+            System.out.println("Enter second two co-ordinates:");
+            x2=sc.nextInt();
+            y2=sc.nextInt();
+            length = Math.sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2);
+            System.out.println("Length of the line:" + length);
+            sc.close();
         }
 }
